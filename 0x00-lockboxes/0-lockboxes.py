008dbd7027b40a1c1b977fda9eed3a97597c5898
Jenni-Foued <medfouedjenni@gmail.com>
2021-07-22 11:35:31 +0100

    pep8 fix

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """This module contains one function: canUnlockAll()"""
 
+
 def canUnlockAll(boxes):
     """Determines if all the boxes can be opened"""
     keysList = [0]
     for key in keysList:
         for k in boxes[key]:
             if k not in keysList:
-                keysList.append(k) 
-    for i in range (len(boxes)):
+                keysList.append(k)
+    for i in range(len(boxes)):
         if i not in keysList:
             return False
     return True
