fa270246ded05cfc66ab6c1bb885588643a52938
Jenni-Foued <medfouedjenni@gmail.com>
2021-08-08 22:14:09 +0100

    First steps

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -1,6 +1,23 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - a function that checks if a node is a leaf
+ *
+ * @node: a pointer to the node to check
+ *
+ * Return: 1 if node is a leaf, otherwise 0 (including node is NULL)
+ */
+int binary_tree_is_leaf(const heap_t *node)
+{
+	if (node)
+	{
+		if (!(node->left) && !(node->right))
+			return (1);
+	}
+	return (0);
+}
+
+/**
  * heap_insert - inserts a value into a Max Binary Heap
  *
  * @root: a double pointer to the root node of the Heap
@@ -10,5 +27,19 @@
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	return;
+	heap_t *new_node;
+
+	if (root == NULL)
+		return (NULL);
+	if ((*root) == NULL)
+	{
+		new_node = binary_tree_node(NULL, value);
+	}
+
+	if (binary_tree_is_leaf(*root))
+	{
+		new_node = binary_tree_node(*root, value);
+	}
+
+	return (new_node);
 }
