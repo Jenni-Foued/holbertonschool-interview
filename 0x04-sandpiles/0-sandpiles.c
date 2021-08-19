116f6372d8edc7cd62464a4a6628fb8f43d21511
Jenni <medfouedjenni@gmail.com>
2021-08-19 20:49:29 +0100

    Sandpiles

---
+++ 0x04-sandpiles/0-sandpiles.c
@@ -0,0 +1,68 @@
+#include "sandpiles.h"
+#include <stdio.h>
+
+/**
+ * check_stability - verify if sandpile is stable or not
+ *
+ * @grid: grid(sandpile) to verify
+ * Return: 1 if stable else 0
+ */
+int check_stability(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid[i][j] > 3)
+					return(0);
+			}
+	}
+	return(1);
+}
+
+/**
+ * sandpiles_sum -  computes the sum of two sandpiles
+ *
+ * @grid1: first sandpile
+ * @grid2: second sandpile
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j;
+	int div4;
+	int stable;
+
+	for (i = 0; i < 3; i++)
+		for (j = 0; j < 3; j++)
+			grid1[i][j] = grid1[i][j] + grid2[i][j];
+
+	stable = check_stability(grid1);
+	while (stable == 0)
+	{
+		printf("=\n");
+    	print_grid(grid1);
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid1[i][j] > 3)
+				{
+					div4 = grid1[i][j] / 4;
+					grid1[i][j] %= 4;
+
+					if (j - 1 >= 0)
+						grid1[i][j - 1] += div4;
+					if (i - 1 >= 0)
+						grid1[i - 1][j] += div4;
+					if (i + 1 < 3)
+						grid1[i + 1][j] += div4;
+					if (j + 1 >= 0)
+						grid1[i][j + 1] += div4;
+				}
+			}
+		}
+		stable = check_stability(grid1);
+	}
+}
