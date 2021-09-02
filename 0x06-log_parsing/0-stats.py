1b2aa747781b23e23400308d0181f79fe9a85157
Jenni <medfouedjenni@gmail.com>
2021-09-02 10:03:36 +0100

    Pep8

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,10 +7,8 @@ import sys
 total_size = 0
 counter = 0
 
-dict_codes_counter = {
-    '200': 0, '301': 0, '400': 0, '401': 0,
-    '403': 0, '404': 0,'405': 0, '500': 0
-}
+dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0,'405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
