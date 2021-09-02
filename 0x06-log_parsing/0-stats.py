f5c69d311670e7799462cc51b40b2bc7d08ea474
Jenni <medfouedjenni@gmail.com>
2021-09-02 07:54:49 +0100

    Quick fix

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -7,7 +7,6 @@ import sys
 total_size = 0
 counter = 0
 # status codes
-codes = ['200', '301', '400', '401', '403', '404', '405', '500']
 dict_codes_counter = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
                 '405': 0, '500': 0}
 
@@ -17,7 +16,7 @@ try:
         if len(line_list) > 2:
             code = line_list[-2]
             size = int(line_list[-1])
-            if code in codes:
+            if code in dict_codes_counter.keys():
                 dict_codes_counter[code] += 1
             total_size += size
             counter += 1
@@ -31,6 +30,7 @@ try:
 
 except Exception:
     pass
+
 finally:
     print("File size: {}".format(total_size))
     for k, v in sorted(dict_codes_counter.items()):
