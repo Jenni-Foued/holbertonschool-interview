ea40c2d32ad3ba50b6db435026d65bbe3bc5f788
Jenni <medfouedjenni@gmail.com>
2021-11-18 12:33:53 +0100

    Draw board

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,22 +1,34 @@
 #!/usr/bin/python3
-"""
-N Queens
-"""
+"""Module to solve the N Queens Problem"""
 import sys
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
+def drawBoard(n, board):
+    """
+    This method draws a board:
+    F means free spot
+    D means danger spot
+    """
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                board[i, j] = 'D'
+            else:
+                board[i,j] = 'F'
 
-N = sys.argv[1]
 
-try:
-    N = int(N)
-except:
-    print('N must be a number')
-    exit(1)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
 
-if N < 4:
-    print('N must be at least 4')
-    exit(1)
+    n = sys.argv[1]
 
+    try:
+        n = int(n)
+    except:
+        print('N must be a number')
+        exit(1)
+
+    if n < 4:
+        print('N must be at least 4')
+        exit(1)
