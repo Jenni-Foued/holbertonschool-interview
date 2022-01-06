75adc2d66223fd9d289006fbc567582eab557030
Franklayn <medfouedjenni@gmail.com>
2022-01-06 04:59:01 +0100

    Done

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -10,7 +10,7 @@ def rotate_2d_matrix(matrix):
     90° degrees clockwise.
     """
     n = len(matrix[0])
-    for i in range (0, n):
+    for i in range(0, n):
         for j in range(i, n):
             temp = matrix[i][j]
             matrix[i][j] = matrix[j][i]
