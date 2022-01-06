d62624345b6e5fb0395bb9a420e3323e54ab994d
Franklayn <medfouedjenni@gmail.com>
2022-01-06 04:12:34 +0100

    Fix syntax errors

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,15 +3,19 @@
 Rotate 2D Matrix
 """
 def rotate_2d_matrix(matrix):
+    """
+    This function rotates a 2d matrix
+    90° degrees clockwise.
+    """
     n = len(matrix[0])
-    for i in range(0,n):
+    for i in range(0, n):
         for j in range(0, i):
-            temp = matrix[i, j]
-            matrix[i, j] = matrix[j, i]
-            matrix[j, i] = temp
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
 
     for i in range(0, n):
-        for j in range(0, n/2):
-            temp = matrix[i, j]
-            matrix[i, j] = matrix[i,n - j - 1]
-            matrix[i,n - j - 1] = temp
+        for j in range(0, n / 2):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n - j - 1]
+            matrix[i][n - j - 1] = temp
