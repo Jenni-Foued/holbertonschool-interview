5c2d64d1c4b50bfac53726afd8ff2a232a3cb179
Franklayn <medfouedjenni@gmail.com>
2022-01-08 23:46:58 +0100

    Test 2

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -1,56 +1,73 @@
 #!/usr/bin/python3
-
-'''
-N queens
-'''
-
-from sys import argv
+""" nqueens """
+import sys
 
 
-def is_NQueen(cell: list) -> bool:
-    '''
-    True if N Queen, False if not
-    '''
-    row_number = len(cell) - 1
-    difference = 0
-    for index in range(0, row_number):
-        difference = cell[index] - cell[row_number]
-        if difference < 0:
-            difference *= -1
-        if difference == 0 or difference == row_number - index:
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    exit(1)
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+N = int(sys.argv[1])
+board = [[0 for i in range(N)]for j in range(N)]
+k = 1
+
+
+def print_sol(board):
+    global k
+    k = k + 1
+    S = []
+    for i in range(N):
+        for j in range(N):
+            if board[i][j] == 1:
+                S.append([i, j])
+    print(S)
+
+
+def isSafe(board, row, col):
+    for i in range(col):
+        if (board[row][i]):
             return False
+    i = row
+    j = col
+    while i >= 0 and j >= 0:
+        if(board[i][j]):
+            return False
+        i -= 1
+        j -= 1
+    i = row
+    j = col
+    while j >= 0 and i < N:
+        if(board[i][j]):
+            return False
+        i = i + 1
+        j = j - 1
     return True
 
 
-def solve_NQeens(dimension: int, row: int, cell: list, outcome: list):
-    """
-    Return result of N Queens recusrivley
-    """
-    # Base case
-    if row == dimension:
-        print(outcome)
-    else:
-        for col in range(0, dimension):
-            cell.append(col)
-            outcome.append([row, col])
-            if (is_NQueen(cell)):
-                solve_NQeens(dimension, row + 1, cell, outcome)
-            cell.pop()
-            outcome.pop()
+def solving(board, col):
+    if (col == N):
+        print_sol(board)
+        return True
+    res = False
+    for i in range(N):
+        if (isSafe(board, i, col)):
+            board[i][col] = 1
+            res = solving(board, col + 1) or res
+            board[i][col] = 0
+    return res
 
 
-if len(argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-try:
-    N = int(argv[1])
-except BaseException:
-    print('N must be a number')
-    exit(1)
-if N < 4:
-    print('N must be at least 4')
-    exit(1)
-else:
-    outcome = []
-    cell = 0
-    solve_NQeens(int(N), cell, [], outcome)
+def solve():
+    board = [[0 for j in range(N)]for i in range(N)]
+    if (solving(board, 0) is False):
+        print("Solution does not exist")
+        return
+    return
+
+
+solve()
