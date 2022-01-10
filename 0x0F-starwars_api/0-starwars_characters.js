03a3965462f02a80a57069f153263c63f5aa8c1d
Franklayn <medfouedjenni@gmail.com>
2022-01-10 23:59:31 +0100

    Test

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,22 +1,23 @@
 #!/usr/bin/node
+
 const request = require('request');
-const id = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${id}`;
-request.get(url, async (error, res) => {
-  if (error) console.log(error);
-  else {
-    const mainErray = JSON.parse(res.body);
-    tryExecuteNext(mainErray.characters, 0);
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+  }
+  response = JSON.parse(body);
+  for (const charId of response.characters) {
+    await new Promise((resolve, reject) => {
+      request(charId, (error, response, body) => {
+        if (error) {
+          console.log(error);
+        }
+        const formatedResponse = JSON.parse(body);
+        console.log(formatedResponse.name);
+        resolve();
+      });
+    });
   }
 });
-
-const tryExecuteNext = (mainErray, index) => {
-  request.get(mainErray[index], (error, res, body) => {
-    if (error) throw error;
-    const result = JSON.parse(body);
-    console.log(result.name);
-    if (index < mainErray.length - 1) {
-      tryExecuteNext(mainErray, index + 1);
-    }
-  });
-};
