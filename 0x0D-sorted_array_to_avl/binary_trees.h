56649a52a757a8428020f1d354edd5f08d885872
Franklayn <medfouedjenni@gmail.com>
2022-01-10 01:09:47 +0100

    Revert

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -3,7 +3,6 @@
 
 #include <stddef.h>
 #include <stdlib.h>
-
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -12,18 +11,22 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+};
 
 typedef struct binary_tree_s binary_tree_t;
+
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
 
+/* print tree function */
 void binary_tree_print(const binary_tree_t *);
+
 avl_t *sorted_array_to_avl(int *array, size_t size);
 
 #endif /* _BINARY_TREES_H_ */
