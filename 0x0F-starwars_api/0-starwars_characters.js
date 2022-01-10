791dfffca222117ee870a24465d0dee1446df462
Franklayn <medfouedjenni@gmail.com>
2022-01-10 23:51:56 +0100

    Done

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,19 +1,27 @@
 #!/usr/bin/node
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
-request(url, async (error, response, body) => {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    for (const charId in characters) {
-      await new Promise((resolve, reject) => {
-        request(characters[charId], (error, response, body) => {
-          if (!error) {
-            console.log(JSON.parse(body).name);
-            resolve();
+const film = process.argv[2];
+let url = 'http://swapi.co/api/people/';
+function filmcharacters (film, url) {
+  request(url, function (err, response, body) {
+    if (err) {
+      console.log(err);
+    } else if (response.statusCode === 200) {
+      let jsonobj = JSON.parse(body);
+      let people = jsonobj.results;
+      for (let i in people) {
+        for (let j in people[i].films) {
+          if (people[i].films[j].includes(film)) {
+            console.log(people[i].name);
           }
-        });
-      });
+        }
+      }
+      if (jsonobj.next !== null) {
+        filmcharacters(film, jsonobj.next);
+      }
+    } else {
+      console.log('An error occured. Status code: ' + response.statusCode);
     }
-  }
-});
+  });
+}
+filmcharacters(film, url);
