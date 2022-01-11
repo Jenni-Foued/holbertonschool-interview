6afff8e237bc78166d0c28b42755092a079abfad
Franklayn <medfouedjenni@gmail.com>
2022-01-11 02:45:24 +0100

    PEP8

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -5,16 +5,16 @@ This module contains one function def rain()
 
 
 def rain(walls):
-	"""Calculate the total amount of rainwater retained"""
-	drops = 0
-	if (walls):
-		for i in range(1, len(walls) - 1):
-			left_wall = walls[i]
-			for j in range(i):
-				left_wall = max(left_wall, walls[j])
-			right_wall = walls[i]
-			for j in range(i + 1, len(walls)):
-				right_wall = max(right_wall, walls[j])
-			drops += (min(left_wall, right_wall) - walls[i])
-		return drops
-	return 0
+    """Calculate the total amount of rainwater retained"""
+    drops = 0
+    if walls and len(walls) > 2:
+        for i in range(1, len(walls) - 1):
+            left_wall = walls[i]
+            for j in range(i):
+                left_wall = max(left_wall, walls[j])
+            right_wall = walls[i]
+            for j in range(i + 1, len(walls)):
+                right_wall = max(right_wall, walls[j])
+            drops += (min(left_wall, right_wall) - walls[i])
+        return drops
+    return 0
