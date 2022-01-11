d350786cb87ee5795622b5a2b32a3b5557c50966
Franklayn <medfouedjenni@gmail.com>
2022-01-11 01:24:09 +0100

    Done

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = `https://swapi-api.hbtn.io/api/films/` + process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 request.get(url, async (error, res) => {
   if (error) console.log(error);
   else {
