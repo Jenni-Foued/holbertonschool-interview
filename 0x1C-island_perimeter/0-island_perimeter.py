2594a98c305f22a61282e54c22e82a3ff3088aa9
Jenni <medfouedjenni@gmail.com>
2022-02-24 20:14:10 +0100

    Fix

--- 0x1C-island_perimeter/0-island_perimeter.py
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -3,25 +3,25 @@
 
 
 def island_perimeter(grid):
-	"""Function returns the perimeter of the island"""
-	perimeter = 0
-	for i in range(len(grid)):
-		for j in range(len(grid[i])):
-			if grid[i][j] == 1:
-				if (j+1) < len(grid[0]) and grid[i][j+1] == 0:
-					perimeter += 1
-				if j == (len(grid[0]) - 1):
-					perimeter += 1
-				if grid[i][j-1] == 0 and j > 0:
-					perimeter += 1
-				if j == 0:
-					perimeter += 1
-				if grid[i-1][j] == 0 and i > 0:
-					perimeter += 1
-				if i == 0:
-					perimeter += 1
-				if (i+1) < len(grid) and grid[i+1][j] == 0:
-					perimeter += 1
-				if i == len(grid) - 1:
-					perimeter += 1
-	return perimeter
+    """Function returns the perimeter of the island"""
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                if (j+1) < len(grid[0]) and grid[i][j+1] == 0:
+                    perimeter += 1
+                if j == (len(grid[0]) - 1):
+                    perimeter += 1
+                if grid[i][j-1] == 0 and j > 0:
+                    perimeter += 1
+                if j == 0:
+                    perimeter += 1
+                if grid[i-1][j] == 0 and i > 0:
+                    perimeter += 1
+                if i == 0:
+                    perimeter += 1
+                if (i+1) < len(grid) and grid[i+1][j] == 0:
+                    perimeter += 1
+                if i == len(grid) - 1:
+                    perimeter += 1
+    return perimeter
