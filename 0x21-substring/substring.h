bae5a8ccfeade2e5f0db4fd081c84947b3524ae2
Jenni <medfouedjenni@gmail.com>
2022-03-30 20:33:05 +0000

    substring

---
+++ 0x21-substring/substring.h
@@ -0,0 +1,10 @@
+#ifndef _SUBSTRING_H_
+#define _SUBSTRING_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif
